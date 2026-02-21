8192e2f5ca9492f7e12c9676e0b7bb374da5a847
harryjuliano <harrywira97@gmail.com>
2026-02-21 13:30:32 +0700

    Fix menu text contrast for light and dark themes

--- resources/js/Components/LinkItem.jsx
+++ resources/js/Components/LinkItem.jsx
@@ -13,9 +13,9 @@ export default function LinkItem({href, icon, link, access, title, sidebarOpen, 
     const hasPermission = auth.super === true || access === true;
 
     // styles links
-    const sideOpen = 'flex items-center font-medium gap-x-3.5 px-4 py-3 hover:border-r-2 capitalize hover:cursor-pointer text-sm text-gray-500 hover:border-r-gray-700 hover:text-gray-900 dark:text-gray-500 dark:hover:border-r-gray-50 dark:hover:text-gray-100';
-    const sideClose = 'min-w-full flex justify-center py-3 hover:border-r-2 hover:cursor-pointer  text-gray-500 hover:border-r-gray-700 hover:text-gray-900 dark:text-gray-500 dark:hover:border-r-gray-50 dark:hover:text-gray-100';
-    const sideActive = 'border-r-2 border-r-gray-400 bg-gray-100 text-gray-700 dark:border-r-gray-500 dark:bg-gray-900 dark:text-white';
+    const sideOpen = 'flex items-center font-medium gap-x-3.5 px-4 py-3 hover:border-r-2 capitalize hover:cursor-pointer text-sm text-gray-700 hover:border-r-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:border-r-gray-50 dark:hover:text-gray-100';
+    const sideClose = 'min-w-full flex justify-center py-3 hover:border-r-2 hover:cursor-pointer text-gray-700 hover:border-r-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:border-r-gray-50 dark:hover:text-gray-100';
+    const sideActive = 'border-r-2 border-r-gray-500 bg-gray-100 text-gray-900 dark:border-r-gray-300 dark:bg-gray-900 dark:text-white';
 
     return (
         <>
