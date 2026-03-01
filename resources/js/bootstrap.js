e668201233e4b029285ebec980f50fdf06532157
harryjuliano <harrywira97@gmail.com>
2026-03-01 15:16:59 +0700

    Fix receiving save 419 by hardening axios CSRF/session config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,9 +2,25 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
 
-const csrfToken = document.head.querySelector('meta[name="csrf-token"]');
+const getMetaCsrfToken = () => document.head.querySelector('meta[name="csrf-token"]')?.content;
 
-if (csrfToken) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken.content;
-}
+const setCsrfHeader = () => {
+    const token = getMetaCsrfToken();
+
+    if (!token) {
+        return;
+    }
+
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+};
+
+setCsrfHeader();
+
+window.axios.interceptors.request.use((config) => {
+    setCsrfHeader();
+
+    return config;
+});
