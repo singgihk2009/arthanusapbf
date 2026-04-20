c4898c4ca2fe5befa8956286e09de3a6fe96376a
harryjuliano <harrywira97@gmail.com>
2026-04-20 23:07:21 +0700

    Redirect root and logout flow to login

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,9 @@ use App\Http\Controllers\Apps\InventoryPostingController;
 use App\Http\Controllers\Apps\Reports\InventoryReportController;
 use App\Http\Controllers\Apps\Integration\IntegrationController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/login');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
