6e498bb41aaae16bb51b874d89c17e549d206491
singgihk2009 <46868841+singgihk2009@users.noreply.github.com>
2026-05-02 18:09:11 +0700

    Refine vendor detail layout for cleaner overview

--- resources/js/Components/Procurement/Vendors/tabs/OverviewTab.jsx
+++ resources/js/Components/Procurement/Vendors/tabs/OverviewTab.jsx
@@ -1,1 +1,8 @@
-export default function Tab({ data }) { return <pre className='text-xs overflow-auto bg-gray-50 p-3 rounded'>{JSON.stringify(data, null, 2)}</pre>; }
+import VendorSummaryCards from '../VendorSummaryCards';
+
+export default function Tab({ data, summary }) {
+  return <div className='space-y-4'>
+    <VendorSummaryCards summary={summary} />
+    <pre className='text-xs overflow-auto bg-gray-50 p-3 rounded'>{JSON.stringify(data, null, 2)}</pre>
+  </div>;
+}
