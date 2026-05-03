f6ced4b2471bfb0227e31db3a044d2902017e992
singgihk2009 <46868841+singgihk2009@users.noreply.github.com>
2026-05-03 16:17:53 +0700

    Remove raw JSON section from vendor overview tab

--- resources/js/Components/Procurement/Vendors/tabs/OverviewTab.jsx
+++ resources/js/Components/Procurement/Vendors/tabs/OverviewTab.jsx
@@ -1,8 +1,7 @@
 import VendorSummaryCards from '../VendorSummaryCards';
 
-export default function Tab({ data, summary }) {
+export default function Tab({ summary }) {
   return <div className='space-y-4'>
     <VendorSummaryCards summary={summary} />
-    <pre className='text-xs overflow-auto bg-gray-50 p-3 rounded'>{JSON.stringify(data, null, 2)}</pre>
   </div>;
 }
