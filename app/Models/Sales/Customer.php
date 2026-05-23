53d7834ddcfffe829bcc3a284503ace5b8a294a2
singgihk2009 <46868841+singgihk2009@users.noreply.github.com>
2026-05-24 01:04:53 +0700

    Fix customer create for legacy required code column

--- app/Models/Sales/Customer.php
+++ app/Models/Sales/Customer.php
@@ -11,7 +11,7 @@ class Customer extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'customer_code','customer_name','customer_type','contact_person','phone','email','address','city','province','postal_code','country','npwp',
+        'code','customer_code','customer_name','customer_type','contact_person','phone','email','address','city','province','postal_code','country','npwp',
         'price_list_id','payment_term_days','credit_limit','salesman_id','status','notes',
     ];
 
